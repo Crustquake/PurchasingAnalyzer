901cf10ad38a144f17216038b923d3870cc2b9a0
Mikhail <mikhailshabalkin@gmail.com>
2020-07-11 12:20:33 +0300

    Initial commit

---
+++ PurchasingAnalyzer.Wpf/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace PurchasingAnalyzer.Wpf
+{
+    internal class Client
+    {
+        public void Run()
+        {
+            try
+            {
+                Window mainWindow = new MainWindow();
+                Application application = new Application();
+                application.Run(mainWindow);
+            }
+            catch (Exception exception)
+            {
+                HandleException(exception);
+            }
+        }
+
+        private void HandleException(Exception exception)
+        {
+            MessageBox.Show($"Fatal error with reason: {exception.Message}. Please look log file", "Fatal error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ PurchasingAnalyzer.Wpf/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PurchasingAnalyzer.Wpf
+{
+    public static class Startup
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            var client = new Client();
+            client.Run();
+        }
+    }
+}
